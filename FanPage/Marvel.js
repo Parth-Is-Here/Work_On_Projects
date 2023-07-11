67887c4b4d4fa0cc412076560613ddea8dae2d39
Parth Kumar Sahu <radhe.radhe.parth.here.18@gmail.com>
2023-07-11 11:33:57 +0530

    git commit
    another one

---
+++ FanPage/Marvel.js
@@ -0,0 +1,14 @@
+window.onload = function() {
+
+  // Login button click event
+  document.getElementById("loginButton").addEventListener("click", function() {
+    // You can add your login logic here
+    alert("Login button clicked!");
+  });
+
+  // Signup button click event
+  document.getElementById("signupButton").addEventListener("click", function() {
+    // You can add your signup logic here
+    alert("Sign up button clicked!");
+  });
+};
